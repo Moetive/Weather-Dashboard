d67a74df384c5821ac825f06a66744f17751cad6
Moetive <Moetive206@gmail.com>
2022-07-11 15:32:06 -0700

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 const apiKey = "&appid=9f103066ad2690dfc98026104a1b9e25"
-const URL = "https://api.openweathermap.org/data/2.5/weather?lat=41.7759&lon=72.5215" + apiKey
+const date = moment()
 
+$("#searchBtn").on("click", () => {
+const cityName = $("#citySearch").val();
+$("#citySearch").val("");
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey
 $.ajax({
     url: URL,
     method: "GET" 
 
 }).then(function(response){
-    console.log(response);
+    console.log(response.weather);
 })
+});
+
+
+// create global variables for city and current date
+// create function
+// inside the function target 
